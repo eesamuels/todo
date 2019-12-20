fbdd8dc582021a9c4dcd08720efc615a61d60a9e
Emma <emma@capturethosetravelers.com>
2019-12-19 16:01:41 -0800

    Set up project more

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import VueFirestore from 'vue-firestore';
 
 Vue.config.productionTip = false
+Vue.use(VueFirestore)
 
 new Vue({
   vuetify,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,39 @@
   <div class="home">
     
     <h1> Goodbye World</h1>
-    <v-btn @click="$router.push({name: 'about'})"> Click Me </v-btn>
+   <v-text-field v-model="newToDo" placeholder="Enter A New To Do"/>
+   <v-btn @click="submitToDo">Submit</v-btn>
+   <br/><br/>
+  <div :key="todo" v-for="todo in todos">
+    <v-checkbox @change="completeToDo(todo)" :label="todo.name"/> 
+  </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { db } from '../firebase';
+export default {
+  data(){
+    return {
+      newToDo: null,
+    }
+  },
+  firestore(){
+    return {
+      todos: db.collection('todos')
+    }
+  },
+  methods:{
+    submitToDo(){
+      this.$firestore.todos.add({name: this.newToDo, complete: false})
+      this.newToDo = null
 
+    },
+    completeToDo(todo){
+      this.$firestore.todos.doc('')
+
+    }
+  }
+}
 </script>
